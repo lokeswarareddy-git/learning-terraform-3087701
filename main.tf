8adc27354e3057530f22863a76ac550693973c1b
Lokeswara Reddy Valipireddy <50443270+lokeswarareddy-git@users.noreply.github.com>
2024-04-18 19:56:21 -0700

    fixed indentation issue with security group rules

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port = 443
   to_port = 443
   protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.id
 }
 
@@ -48,6 +48,6 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port = 0
   to_port = 0
   protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.id
 }
