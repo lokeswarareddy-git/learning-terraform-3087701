8eab77beab9b58fb708c866192baa6395beffd0f
Lokeswara Reddy Valipireddy <50443270+lokeswarareddy-git@users.noreply.github.com>
2024-04-18 19:58:30 -0700

    Fixed the issue with securitygroup

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   to_port = 443
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
@@ -49,5 +49,5 @@ resource "aws_security_group_rule" "blog_http_in" {
   to_port = 0
   protocol = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.id
+  security_group_id = aws_security_group.blog.id
 }
